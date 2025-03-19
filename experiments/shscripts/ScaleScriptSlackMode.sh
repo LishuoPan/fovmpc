061c2fc8e0cc9a6ef3c0bdec93adaffb9048a7da
Lishuo Pan <pls19961219@gmail.com>
2025-03-19 17:24:13 -0400

    update the slack script

--- experiments/shscripts/ScaleScriptSlackMode.sh
+++ experiments/shscripts/ScaleScriptSlackMode.sh
@@ -6,29 +6,21 @@ instances="circle"
 min_r=2
 max_r=3
 max_exp=2
-min_fov=120
-max_fov=360
-fov_step=120
-min_slack_decay=0.2
-max_slack_decay=0.3
-slack_decay_step=0.1
 default_slack_decay=0.2
 for (( exp_idx = 0; exp_idx < ${max_exp}; exp_idx++ )); do
   (for (( num_r = ${min_r}; num_r <= ${max_r}; num_r++ )); do
-    for fov in 120 240 360; do
+    for fov in 120 240; do
         echo experiment instance type:${instances} num_robot:${num_r}, exp_idx:${exp_idx}
-        ./mpc_cbf_examples_BezierIMPCCBFPFXYYaw_example --instance_type ${instances} --num_robots ${num_r} --fov ${fov} --slack_decay ${default_slack_decay} --write_filename "/media/lishuo/ssd/RSS2025_results/log${date}/${instances}${num_r}_fov${fov}_decay${default_slack_decay}_States_${exp_idx}.json"
+        ./mpc_cbf_examples_BezierIMPCCBFPFXYYaw_example --instance_type ${instances} --num_robots ${num_r} --fov ${fov} --slack_mode 0 --slack_decay ${default_slack_decay} --write_filename "/media/lishuo/ssd/RSS2025_results/log${date}/${instances}${num_r}_fov${fov}_slack_off_States_${exp_idx}.json"
     done
   done) &
 
   (for (( num_r = ${min_r}; num_r <= ${max_r}; num_r++ )); do
-    for fov in 120 240; do
-      for slack_decay in 0.1; do
+      for fov in 120 240; do
           echo experiment instance type:${instances} num_robot:${num_r}, exp_idx:${exp_idx}
-          ./mpc_cbf_examples_BezierIMPCCBFPFXYYaw_example --instance_type ${instances} --num_robots ${num_r} --fov ${fov} --slack_decay ${slack_decay} --write_filename "/media/lishuo/ssd/RSS2025_results/log${date}/${instances}${num_r}_fov${fov}_decay${slack_decay}_States_${exp_idx}.json"
+          ./mpc_cbf_examples_BezierIMPCCBFPFXYYaw_example --instance_type ${instances} --num_robots ${num_r} --fov ${fov} --slack_decay ${default_slack_decay} --write_filename "/media/lishuo/ssd/RSS2025_results/log${date}/${instances}${num_r}_fov${fov}_decay${default_slack_decay}_States_${exp_idx}.json"
       done
-    done
-  done) &
+    done) &
 
 done
 wait # Wait for all background processes to complete
